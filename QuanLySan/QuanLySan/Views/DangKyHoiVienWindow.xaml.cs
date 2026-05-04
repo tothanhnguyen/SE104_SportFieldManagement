6863985ca51b68677c752b6a11479531232745cb
Ncyntrq <quocnguyen5026@gmail.com>
2026-05-04 12:11:21 +0700

    giao dien dang ky hoi vien

---
+++ QuanLySan/QuanLySan/Views/DangKyHoiVienWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace QuanLySan.Views
+{
+    public partial class DangKyHoiVienWindow : Window
+    {
+        public DangKyHoiVienWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Kéo thả cửa sổ khi click vào Header
+        private void Header_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        // Tắt cửa sổ
+        private void BtnThoat_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
