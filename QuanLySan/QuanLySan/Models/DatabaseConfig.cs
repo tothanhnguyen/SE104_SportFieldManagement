3b056179cb7c29b6c8b15cd24b32cd5a42724f8c
Hoang Nguyen <nguyen1007k6@gmail.com>
2026-06-28 11:13:35 +0700

    Hoan thien man hinh chinh

--- QuanLySan/QuanLySan/Models/DatabaseConfig.cs
+++ QuanLySan/QuanLySan/Models/DatabaseConfig.cs
@@ -3,6 +3,6 @@ namespace QuanLySan.Models
     public static class DatabaseConfig
     {
         // Khai báo public static để dùng chung toàn dự án
-        public static string ConnectionString = @"Data Source=DESKTOP-LOJ3INE\SQLEXPRESS;Initial Catalog=QLSanTheThao;Integrated Security=True;Encrypt=True;TrustServerCertificate=True;";
+        public static string ConnectionString = @"Data Source=localhost;Initial Catalog=QLSanTheThao;Integrated Security=True;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Encrypt=False;TrustServerCertificate=True;Command Timeout=30";
     }
 }
