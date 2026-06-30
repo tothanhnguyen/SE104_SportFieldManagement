976088c382c96ff5ae4aa1239df12e6f19049337
Hoang Nguyen <nguyen1007k6@gmail.com>
2026-06-30 15:19:43 +0700

    fix thay doi quy dinh

--- QuanLySan/QuanLySan/Views/ThayDoiQuyDinhWindow.xaml.cs
+++ QuanLySan/QuanLySan/Views/ThayDoiQuyDinhWindow.xaml.cs
@@ -8,7 +8,29 @@ namespace QuanLySan.Views
         public ThayDoiQuyDinhWindow()
         {
             InitializeComponent();
-            DataContext = new ThayDoiQuyDinhViewModel();
+            var vm = new ThayDoiQuyDinhViewModel();
+            DataContext = vm;
+            
+            // Theo dõi khi DanhSachLoaiHoiVien có item mới để scroll và focus
+            vm.DanhSachLoaiHoiVien.CollectionChanged += (s, e) =>
+            {
+                if (e.Action == System.Collections.Specialized.NotifyCollectionChangedAction.Add && e.NewItems != null && e.NewItems.Count > 0)
+                {
+                    var newItem = e.NewItems[0];
+                    
+                    // Dispatcher để đảm bảo UI đã render xong row mới
+                    Dispatcher.InvokeAsync(() =>
+                    {
+                        dgLoaiHoiVien.SelectedItem = newItem;
+                        dgLoaiHoiVien.ScrollIntoView(newItem);
+                        
+                        // Focus vào ô đầu tiên có thể edit (Tên hạng)
+                        dgLoaiHoiVien.CurrentCell = new System.Windows.Controls.DataGridCellInfo(newItem, dgLoaiHoiVien.Columns[1]);
+                        dgLoaiHoiVien.Focus();
+                        dgLoaiHoiVien.BeginEdit();
+                    }, System.Windows.Threading.DispatcherPriority.Background);
+                }
+            };
         }
 
         private void BtnThoat_Click(object sender, RoutedEventArgs e)
